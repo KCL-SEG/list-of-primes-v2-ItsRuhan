8240ed18e4a2ff75b35d8c496bdab00c2118dec9
Ruhan Mohammad Salam <k21056749@kcl.ac.uk>
2022-12-09 13:42:28 +0000

    Update primes.py

--- primes.py
+++ primes.py
@@ -2,5 +2,22 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
+    
+    if(number_of_primes<1):
+        raise ValueError("Value must be more than zero")
+
     list = []
+
+    i = 0
+    number = 2
+
+    while i < number_of_primes:
+        for n in range(2, number):
+            if(number % n == 0):
+                break
+        else:   
+            list.append(number)
+            i += 1                   
+        number += 1
+
     return list
